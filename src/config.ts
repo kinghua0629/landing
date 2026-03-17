114ef2abe386a32ae17d4af13ef152afd142165c
King Hua <kinghua0629@outlook.com>
2026-03-17 23:12:20 +0800

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,23 @@
+export const SITE = {
+  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  author: "King Hua",
+  profile: "https://kinghua0629.com/",
+  desc: "My personal landing page",
+  title: "King's Protofolio",
+  ogImage: "",
+  lightAndDarkMode: true,
+  postPerIndex: 4,
+  postPerPage: 4,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  showArchives: true,
+  showBackButton: true, // show back button in post detail
+  editPost: {
+    enabled: true,
+    text: "Edit page",
+    url: "https://github.com/kinghua0629/landing/edit/main/",
+  },
+  dynamicOgImage: true,
+  dir: "auto", // "rtl" | "auto"
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  timezone: "Asia/Shanghai", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+} as const;

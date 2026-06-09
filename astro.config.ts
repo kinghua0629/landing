9e3a5f3579e63e92c1b3f2b4cbc5a6e2561f6aa9
King Hua <kinghua0629@outlook.com>
2026-06-09 18:24:12 +0800

    modify shikiConfig in astro.config.ts
    Signed-off-by: King Hua <kinghua0629@outlook.com>

--- astro.config.ts
+++ astro.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     }),
     shikiConfig: {
       // For more themes, visit https://shiki.style/themes
-      themes: { light: "min-light", dark: "night-owl" },
+      themes: { light: "github-light-default", dark: "github-dark-default" },
       defaultColor: false,
       wrap: false,
       transformers: [
